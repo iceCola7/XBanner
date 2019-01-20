9bf863fb97efbf108d6274744ad16464d7dfafa7
chenxz <563859095@qq.com>
2019-01-20 15:13:26 +0800

    commit

---
+++ xbanner/src/main/java/com/cxz/xbanner/XBannerUtil.java
@@ -0,0 +1,47 @@
+package com.cxz.xbanner;
+
+import android.content.Context;
+import android.content.res.Resources;
+import android.graphics.drawable.Drawable;
+import android.graphics.drawable.StateListDrawable;
+import android.util.DisplayMetrics;
+import android.util.TypedValue;
+
+/**
+ * @author chenxz
+ * @date 2019/1/20
+ * @desc
+ */
+public class XBannerUtil {
+
+    /**
+     * 设置背景选择器
+     *
+     * @param pressedDraw
+     * @param normalDraw
+     * @return
+     */
+    public static StateListDrawable getSelector(Drawable normalDraw, Drawable pressedDraw) {
+        StateListDrawable stateListDrawable = new StateListDrawable();
+        stateListDrawable.addState(new int[]{android.R.attr.state_enabled}, pressedDraw);
+        stateListDrawable.addState(new int[]{}, normalDraw);
+        return stateListDrawable;
+    }
+
+    public static int dp2px(Context context, float dpValue) {
+        return (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, dpValue, context.getResources().getDisplayMetrics());
+    }
+
+    public static int sp2px(Context context, float spValue) {
+        return (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_SP, spValue, context.getResources().getDisplayMetrics());
+    }
+
+    public static int getScreenWidth(Context context) {
+        Resources resources = context.getResources();
+        DisplayMetrics dm = resources.getDisplayMetrics();
+        int width = dm.widthPixels;
+        return width;
+    }
+
+
+}
