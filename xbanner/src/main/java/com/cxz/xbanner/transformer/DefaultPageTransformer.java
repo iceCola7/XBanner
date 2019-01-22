e016f37aac07d91a9ad554ebf134a21057597d66
chenxz <563859095@qq.com>
2019-01-22 21:07:54 +0800

    commit

---
+++ xbanner/src/main/java/com/cxz/xbanner/transformer/DefaultPageTransformer.java
@@ -0,0 +1,25 @@
+package com.cxz.xbanner.transformer;
+
+import android.view.View;
+
+/**
+ * @author chenxz
+ * @date 2019/1/20
+ * @desc
+ */
+public class DefaultPageTransformer extends XPageTransformer {
+    @Override
+    public void handleInvisiblePage(View view, float position) {
+
+    }
+
+    @Override
+    public void handleLeftPage(View view, float position) {
+
+    }
+
+    @Override
+    public void handleRightPage(View view, float position) {
+
+    }
+}
---
+++ xbanner/src/main/java/com/cxz/xbanner/transformer/TransitionEffect.java
@@ -0,0 +1,16 @@
+package com.cxz.xbanner.transformer;
+
+/**
+ * @author chenxz
+ * @date 2019/1/20
+ * @desc
+ */
+public enum TransitionEffect {
+    Default,
+    Alpha,
+    Rotate,
+    Cube,
+    Accordion,
+    Depth,
+    Zoom
+}
